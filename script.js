926b1d2606cb217209b43710f7959eb7014cbe53
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-03-03 10:55:51 +0300

    temperature ranges are more specified

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
     let tempInput = document.getElementById("Temperature");
 
     function clicked() {
-        let temp = parseFloat(tempInput.value); 
+        let temp = parseFloat(tempInput.value); // Get the value and convert it to a number
 
         if (isNaN(temp)) {
             resultDiv.innerText = "Please enter a valid number";
-        } else if (temp <= 0) {
-            resultDiv.innerText = "Very cold";
-        } else if (temp >= 1 && temp <= 25) {
-            resultDiv.innerText = "Temperature is moderate";
-        } else {
-            resultDiv.innerText = " Temperature  is Very hot";
+        } else if (temp <= -273) {
+            resultDiv.innerText = "Extremely cold and beyond absolute zero";
+        } else if (temp >= -272 && temp <= -125) {
+            resultDiv.innerText = "Temperature is Very cold";
+        } else if(temp >= -124 && temp <= 0){
+            resultDiv.innerText = "cold";
+        } else if(temp >=1 && temp <=30){
+            resultDiv.innerText = "Morderate temperature";
+        } else if(temp >=31 && temp <=50){
+            resultDiv.innerText = "Temperature is hot";
+        }else{
+            resultDiv.innerText = "Extremely hot";
         }
     }
 
